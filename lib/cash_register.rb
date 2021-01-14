8a4cc789ac8a976131066989766998d9385cc689
HannaBM12 <Hannabmulugeta@gmail.com>
2021-01-13 19:58:45 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require 'pry'
+class CashRegister
+    attr_accessor :discount, :total, :title, :price
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        # binding.pry
+    end
 
+    def add_item(title, price, quantity = 1)
+        # binding.pry
+        @total += price * quantity
+
+    end
+    def apply_discount
+        @total = @total * (1- @discount/100)
+        # binding.pry
+    end
+
+end
