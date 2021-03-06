ed33b970312af1c3ebaf6bf39a9f6c06b70e1356
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-05 22:20:00 -0500

    commit tree fae5d1b581b04345ee244156820b3ea5d321d0c9

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,41 +1,94 @@
 require 'pry'
+
 class CashRegister
-    attr_reader :discount, :item
-    attr_accessor :total
-    def initialize(discount = 0)
-        @total = 0
+    attr_accessor :total, :discount
+
+    def initialize(total = 0, discount = 20)
+        @total = total
         @discount = discount
         @item = []
-        @price = []
-        # binding.pry
     end
 
-    def add_item(title, price, quantity = 1)
-        # binding.pry
+    def add_item(item, price, quantity = 1)
         @total += price * quantity
-        quantity.times do
-            item << title
-        end
-        @last_transaction = [price, quantity]
+        @item << item
+        @total
     end
 
     def apply_discount
-        if @discount > 0
-            @total = (@total * (1 - @discount/100.0)).to_i
-            # binding.pry
-            return "After the discount, the total comes to $#{@total}."
+        if @discount >0
+            @total = @total * (1- @discount/100.00).to_i
+            puts "After the discount, the total comes to $800." 
         else
-            return "There is no discount to apply."
+            puts "There is no discount to apply."
         end
+
     end
 
-    def items
-        @item
-    end
 
-    def void_last_transaction
-        @total -= @last_transaction[0]*@last_transaction[1]
-        # binding.pry
-    end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'pry'
+# class CashRegister
+#     attr_reader :discount, :item
+#     attr_accessor :total
+#     def initialize(discount = 0)
+#         @total = 0
+#         @discount = discount
+#         @item = []
+#         @price = []
+#         # binding.pry
+#     end
+
+#     def add_item(title, price, quantity = 1)
+#         # binding.pry
+#         @total += price * quantity
+#         quantity.times do
+#             item << title
+#         end
+#         @last_transaction = [price, quantity]
+#     end
+
+#     def apply_discount
+#         if @discount > 0
+#             @total = (@total * (1 - @discount/100.0)).to_i
+#             binding.pry
+#             return "After the discount, the total comes to $#{@total}."
+#         else
+#             return "There is no discount to apply."
+#         end
+#     end
+
+#     def items
+#         @item
+#     end
+
+#     def void_last_transaction
+#         @total -= @last_transaction[0]*@last_transaction[1]
+#         # binding.pry
+#     end
+
+# end
